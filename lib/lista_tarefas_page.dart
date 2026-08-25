46444d1840c536775b8c9302bba1099048c9616e
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-08-25 17:09:43 -0300

    segundoCommit

--- lib/lista_tarefas_page.dart
+++ lib/lista_tarefas_page.dart
@@ -11,18 +11,69 @@ class ListaTarefasPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: ListView(
-        children: const [
+        padding: EdgeInsets.all(12),
+        children: [
           Card(
             child: ListTile(
               leading: Icon(
                 Icons.check_circle,
                 color: Colors.green,
               ),
-              title: Text('Configurar o ambiente de desenvolvimento'),
+              title: Text(
+                'Configurar o ambiente de desenvolvimento',
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
               subtitle: Text('Concluída'),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.circle_outlined,
+                color: Colors.grey,
+              ),
+              title: Text(
+                'fazer a atividade flutter',
+              ),
+              subtitle: Text('Pendente'),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.check_circle,
+                color: Colors.green,
+              ),
+              title: Text(
+                'Fiz 126 perguntas no Sumário documentado da Alexandra',
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+              subtitle: Text('Concluída'),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
       ),
     );
   }
