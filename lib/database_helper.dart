41434237e3498606ed426d1edd24fa8ca2302326
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-09-09 15:17:26 -0300

    FaseeD

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -4,6 +4,7 @@ import 'package:sqflite/sqflite.dart';
 class DatabaseHelper {
   static Database? _db;
 
+  //Abre ou cria, se existe o arquivo no banco de dados
   static Future<Database> abrirBanco() async {
     final caminho = join(await getDatabasesPath(), 'tarefas.db');
 
@@ -13,7 +14,7 @@ class DatabaseHelper {
       onCreate: (db, version) {
         return db.execute(
           'CREATE TABLE tarefas ('
-          'id INTEGER PRIMEARY KEY AUTOINCREMENT,'
+          'id INTEGER PRIMARY KEY AUTOINCREMENT,'
           'titulo TEXT'
           'situacao INTEGER' //0 = false, 1 = true
           ')',
@@ -22,8 +23,26 @@ class DatabaseHelper {
     );
   }
 
+  //Getter que devolve o banco de dados já aberto, ou abre se ainda não existe
   static Future<Database> get database async {
     _db ??= await abrirBanco();
     return _db!;
+  }
+
+  //READ, Buscar todas as tarefas salvas no Banco
+  static Future<List<Map<String, dynamic>>> buscarTarefas() async {
+    final db = await DatabaseHelper.database;
+    return db.query('tarefas'); //SELECT * FROM TAREFAS
+  }
+
+  //CREATE: Inserir uma nova tarefa no banco.
+  static Future<void> inserirTarefa(
+    String titulo,
+  ) async {
+    final db = await DatabaseHelper.database;
+    await db.insert('tarefas', {
+      'titulo': titulo,
+      'situacao': 0,
+    });
   }
 }
--- lib/lista_tarefas_page.dart
+++ lib/lista_tarefas_page.dart
@@ -1,54 +1,105 @@
+import 'package:app_listatarefas/database_helper.dart';
 import 'package:flutter/material.dart';
 
-class ListaTarefasPage extends StatelessWidget {
+class ListaTarefasPage extends StatefulWidget {
   const ListaTarefasPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> tarefas = [
-      {'titulo': 'Fazer Compras', 'situacao': false},
-      {'titulo': 'Pagar Conta de Luz', 'situacao': true},
-      {'titulo': 'Revisar aula de TI', 'situacao': true},
-      {'titulo': 'Pagar Fatura do Inter', 'situacao': false},
-      {'titulo': 'Levar carro na manutenção', 'situacao': false},
-    ];
+  State<ListaTarefasPage> createState() => _ListaTarefasPageState();
+}
 
+class _ListaTarefasPageState extends State<ListaTarefasPage> {
+  List<Map<String, dynamic>> tarefas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    carregarTarefas();
+  }
+
+  void carregarTarefas() async {
+    final dados = await DatabaseHelper.buscarTarefas();
+    setState(() {
+      tarefas = dados;
+    });
+  }
+
+  void adicionarTarefa() {
+    final novaTarefaController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Nova tarefa'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: novaTarefaController,
+                decoration: InputDecoration(hintText: 'Digite o título...'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                //Função pra fechar qualquer janela/tela
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Adicionar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Minhas Tarefas"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(12),
-        itemCount: tarefas.length,
-        itemBuilder: (context, index) {
-          final tarefa = tarefas[index];
-          final bool situacao = tarefa['situacao'];
-          return Card(
-            child: ListTile(
-              leading: Icon(
-                situacao ? Icons.check_circle : Icons.circle_outlined,
-                color: situacao ? Colors.green : Colors.grey,
-              ),
-              title: Text(
-                tarefa['titulo'],
-                style: TextStyle(
-                  decoration: situacao
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-              subtitle: Text(situacao ? 'Concluída' : 'Pendente'),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
+      body: tarefas.isEmpty
+          ? Center(
+              child: Text('Nenhuma tarefa ainda. Toque em + para adicionar.'),
+            )
+          : ListView.builder(
+              padding: EdgeInsets.all(12),
+              itemCount: tarefas.length,
+              itemBuilder: (context, index) {
+                final tarefa = tarefas[index];
+                final bool situacao = tarefa['situacao'];
+                return Card(
+                  child: ListTile(
+                    leading: Icon(
+                      situacao ? Icons.check_circle : Icons.circle_outlined,
+                      color: situacao ? Colors.green : Colors.grey,
+                    ),
+                    title: Text(
+                      tarefa['titulo'],
+                      style: TextStyle(
+                        decoration: situacao
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                      ),
+                    ),
+                    subtitle: Text(situacao ? 'Concluída' : 'Pendente'),
+                    trailing: Icon(
+                      Icons.delete_outline,
+                      color: Colors.grey,
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => adicionarTarefa(),
         backgroundColor: Colors.indigo,
         foregroundColor: Colors.white,
         child: Icon(Icons.add),
